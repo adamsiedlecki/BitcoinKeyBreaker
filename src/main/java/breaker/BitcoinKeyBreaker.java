732c2cc4c5ffc52b0f65a094a88bd1e37d7e1748
adamsiedlecky <adamsiedlec@wp.pl>
2019-05-02 23:24:35 +0200

    I have deleted spring boot adnotation becouse it wasn't used.

--- src/main/java/breaker/BitcoinKeyBreaker.java
+++ src/main/java/breaker/BitcoinKeyBreaker.java
@@ -2,18 +2,15 @@ package breaker;
 
 import breaker.beans.ConsoleUI;
 import breaker.configuration.Conf;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 // Made by AdamSiedlecky
-@SpringBootApplication
 public class BitcoinKeyBreaker {
 
     public static void main (String [] args){
 
-    SpringApplication.run(BitcoinKeyBreaker.class);
-    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Conf.class);
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Conf.class);
 
         ConsoleUI consoleUI = context.getBean("consoleUI",ConsoleUI.class);
         consoleUI.getStarted();
